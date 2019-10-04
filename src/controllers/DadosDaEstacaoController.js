0d840b8b1caf0b554e95cc2c1b76f77d1abd784b
ArrthurGeronimo <artha.desenvolvimento@gmail.com>
2019-10-04 17:54:25 -0400

    teste

--- src/controllers/DadosDaEstacaoController.js
+++ src/controllers/DadosDaEstacaoController.js
@@ -4,8 +4,10 @@ const DadosDaEstacao = mongoose.model('DadosDaEstacao');
 
 module.exports = {
 	async index(req, res){
-		const dadosDaEstacao = await DadosDaEstacao.find();
-		return res.json(dadosDaEstacao);
+
+		//const dadosDaEstacao = await DadosDaEstacao.find();
+		//return res.json(dadosDaEstacao);
+		return res.json('TESTE')
 	},
 
 	async show(req, res){
